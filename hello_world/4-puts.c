13db53c0db1dfb1496023ad8702bfae7513b968f
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-13 01:38:12 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,9 @@
 #!/bin/bash
-puts (Programming is like building a multilingual puzzle);
-	return 0;
+#include <stdio.h>
+
+int main() 
+{
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
+}
+
