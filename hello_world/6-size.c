43a44e8854fa177bb1af54933aec2a7d656161f2
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-13 02:39:14 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -7,7 +7,11 @@
 
 int main(void)
 {
-printf("Size of a char: %u bytes", sizeof(char));
+printf("Size of a char: %lu bytes", sizeof(char));
+printf("Size of a int: %lu bytes", sizeof(int));
+printf("Size of a long int: %lu bytes", sizeof(long int));
+printf("Size of a long long int: %lu bytes", sizeof(long long int));
+printf("Size of a float: %lu bytes", sizeof(float));
 
 return (0);
 }
