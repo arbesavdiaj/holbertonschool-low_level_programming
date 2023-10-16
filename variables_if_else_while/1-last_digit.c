95300b305d5168b016d102c92bae80de5abc506e
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-16 13:11:13 -0700

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,11 +1,9 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 int main(void) {
     int n;
