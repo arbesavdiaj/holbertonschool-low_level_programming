5b48ce7c6af15ee8dfeb0f9898b2ec61dc632ec6
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-17 03:26:18 -0700

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ *  * main - entry point
+ *   * Return: 0
+ */
 int main(void)
 {
 	char i;
