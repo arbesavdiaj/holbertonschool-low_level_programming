a7a7f9fc8394b3c2acc6ad5036b37e3960a6bfc3
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-17 06:28:13 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *  * main - entry point
+ *   * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+	if (a < 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
