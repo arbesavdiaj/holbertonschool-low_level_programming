83dcf3c39cd129a97e88d3fc86a05ee010290dd4
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-17 03:40:06 -0700

    4-print_alphabt.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *  * main - entry point
+ *   * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	if (i != 'q' && i != 'e')
+		putchar(i);
+		putchar('\n');
+
+	return (0);
+}
