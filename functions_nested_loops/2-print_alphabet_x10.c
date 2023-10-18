761a807954cdec97e330803bff17d82dbdf08a61
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-18 02:49:33 -0700

    2-alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int b;
+
+	for (b = 0; b < 10; b++)
+	{
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	}
+	 _putchar('\n');
+}
