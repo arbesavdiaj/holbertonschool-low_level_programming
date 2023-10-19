0d73d7830a0b14c34ac3d9704e8ec18343745ae8
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-19 09:32:26 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers
+ * Return: numbers from 1 to 9
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		_putchar(n + 48);
+	_putchar('\n');
+}
