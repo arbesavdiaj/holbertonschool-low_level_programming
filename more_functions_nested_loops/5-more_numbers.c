b1a1c377f5c7c68c14438a0a115b2a754cd03054
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-20 02:01:31 -0700

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers
+ * Return: numbers from 0 to 14 followed by a new line
+ */
+void more_numbers(void)
+{
+	int n;
+	int m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m <= 14; m++)
+		{
+			if (m > 9)
+			{
+				_putchar(m / 10 + '0');
+			}
+				_putchar(m % 10 + '0');
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
