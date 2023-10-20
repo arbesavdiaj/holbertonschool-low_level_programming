4f1ac5fc02070c8503e95cbdbe775d82dacf640e
Arbes14 <arbesavdiaj14@gmail.com>
2023-10-20 04:13:53 -0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints the numbers
+ * @n: parameter from input until when print
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
