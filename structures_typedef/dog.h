6a6feb106f0c6a681f5db60fe252e0c92ba78bff
Arbes14 <arbesavdiaj14@gmail.com>
2023-11-13 01:42:58 -0800

    Define a new type struct dog with the following elements

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Short description
+ * @name: hello
+ * @age: age
+ * @owner: owner
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
