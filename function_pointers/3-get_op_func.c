44be8c4029a612516dd2c74944a6e7fad3c2201a
Arbes14 <arbesavdiaj14@gmail.com>
2023-11-15 02:43:10 -0800

    calculator

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -23,13 +23,12 @@ int (*get_op_func(char *s))(int, int)
 
 	while (ops[i].op != NULL)
 	{
-		if (*s == *ops[i].op)
+		if (strcmp(ops[i].op, s) == 0)
 		{
 			return (ops[i].f);
 		}
 		i++;
 	}
-	printf("Error\n");
-	exit(99);
+	return (NULL);
 }
 
