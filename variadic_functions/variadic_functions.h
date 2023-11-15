56d9eddfafa5f7cddbf26295491230487a72ab41
Arbes14 <arbesavdiaj14@gmail.com>
2023-11-15 07:57:42 -0800

    Write a function that returns the sum of all its parameters

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
 #ifndef VARIADIC_FUNCTIONS
-#define VARIADUC_FUNCTIONS
+#define VARIADIC_FUNCTIONS
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
