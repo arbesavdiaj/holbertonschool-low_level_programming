6117d6104f7899df8a4d9deea1f763c5e7cb82cf
Arbes14 <arbesavdiaj14@gmail.com>
2023-11-15 02:09:50 -0800

    calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -21,7 +21,13 @@ int main(int argc, char *argv[])
     }
     num1 = atoi(argv[1]);
     num2 = atoi(argv[3]);
+
     operation = get_op_func(argv[2]);
+    if (operation == NULL)
+    {
+	    printf("Error\n");
+	    exit(99);
+    }
     printf("%d\n", operation(num1, num2));
     return (0);
 }
