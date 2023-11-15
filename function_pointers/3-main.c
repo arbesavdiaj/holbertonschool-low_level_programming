8e2d3ee57e223d002a1e9be3e6f7b9e836d22205
Arbes14 <arbesavdiaj14@gmail.com>
2023-11-15 02:44:08 -0800

    calculator

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -1,18 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * main - performs simple operations
- * @argc: number of arguments passed
- * @argv: array of pointers to the arguments
- *
- * Return: always 0
+ * main - Entry point. Performs basic arithmetic operations on two numbers.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ * Return: Always 0 (Success).
  */
+
 int main(int argc, char *argv[])
 {
 	int num1, num2;
-	int (*operation)(int, int);
+	int (*op)(int, int);
 
 	if (argc != 4)
 	{
@@ -21,8 +19,17 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	operation = get_op_func(argv[2]);
-	printf("%d\n", operation(num1, num2));
+	op = get_op_func(argv[2]);
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", op(num1, num2));
 	return (0);
 }
-
