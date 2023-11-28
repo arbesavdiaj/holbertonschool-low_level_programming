7b338c242a6dbf92458a42a25f20e4d29e803146
Arbes14 <arbesavdiaj14@gmail.com>
2023-11-28 07:53:24 -0800

    Write a function that adds a new node at the end of a list_t list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,24 +10,14 @@
  *
  * Return: address of the new element, or NULL if it failed
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
-	if (str == NULL)
-		return (NULL);
-
 	if (new == NULL)
-		exit(EXIT_FAILURE);
-
+		return (NULL);
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		exit(EXIT_FAILURE);
-	}
-	new->len = strlen(str);
-
 	new->next = *head;
 	*head = new;
 
