a730f4b8f5272c7721c106e1b550e78dfe23e711
Arbes14 <arbesavdiaj14@gmail.com>
2023-11-30 02:49:15 -0800

    Write a function that returns the sum of all the data (n) of a dlistint_t linked list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * sum_dlistint - function
+ * @head: head
+ * Return: Sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+	dlistint_t *current = head;
+
+	while (current != NULL)
+	{
+		current += current->n;
+		current = current->next;
+	}
+	return (sum);
+}
