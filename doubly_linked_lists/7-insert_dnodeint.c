e9eef787fb4e9ec38a9235f4f090570aa8947f19
Arbes14 <arbesavdiaj14@gmail.com>
2023-12-01 01:00:10 -0800

    Write a function that inserts a new node at a given position

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,37 +1,52 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position
- * @h: pointer to the first node in the list
- * @idx: index of the list where the new node should be added
- * @n: data to insert in that new node
- * Return: pointer to the new node, or NULL if it fails
-*/
+ * insert_dnodeint_at_index - Inserts a new node at a given position.
+ * @h: Double pointer to the first node of the double linked list.
+ * @idx: Index of the list where the new node should be added.
+ * @n: Integer to be included in the new node.
+ *
+ * Return: Address of the new node, or NULL if it failed.
+ */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *temp = *h;
+	dlistint_t *new_node, *temp = *h;
 	unsigned int i = 0;
 
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+	if (h == NULL)
 		return (NULL);
-	new->n = n;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-	while (i < idx - 1)
-	{
-		temp = temp->next;
-		i++;
-	}
-	if (temp->next == NULL)
-	{
-		return (add_dnodeint_end(h, n));
-	}
-	new->next = temp->next;
-	new->prev = temp;
-	temp->next->prev = new;
-	temp->next = new;
 
-	return (new);
+	while (i++ < idx)
+	{
+		if (temp == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		if (i == idx)
+		{
+			new_node->next = temp->next;
+			new_node->prev = temp;
+			if (temp->next != NULL)
+				temp->next->prev = new_node;
+			temp->next = new_node;
+			return (new_node);
+		}
+		temp = temp->next;
+	}
+	free(new_node);
+	return (NULL);
 }
