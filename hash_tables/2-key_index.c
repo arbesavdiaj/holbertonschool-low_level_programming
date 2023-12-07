15c5e46f6272c355a658e4adfac8a099fd655755
Arbes14 <arbesavdiaj14@gmail.com>
2023-12-07 14:08:53 -0800

    Write a function that adds an element to the hash table

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -9,10 +9,5 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	int hash_value;
-	int index;
-
-	hash_value = hash_djb2(key);
-	index = hash_value % size;
-		return (index);
+	return (hash_djb2(key) % size);
 }
