2033456d2e7f41a4d0c3dd5e9ea3d1c814059021
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-01-12 17:26:11 +0100

    searches for a value in an array of int

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -20,7 +20,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("%d\n", array[i]);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	
 		if (array[i] == value)
 		{
